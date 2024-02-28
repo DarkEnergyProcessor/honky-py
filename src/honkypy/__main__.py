a340726f189ff2b3c3f8c2eb1871236070b45470
Miku AuahDark <lumi.hotaru@gmail.com>
2024-02-28 13:57:50 +0800

    Add command-line option to encrypt files.
    Closes #1.

--- src/honkypy/__main__.py
+++ src/honkypy/__main__.py
@@ -22,38 +22,64 @@
 
 import argparse
 import io
+import os.path
 
-from . import decrypt_setup_probe, StreamIOWrapper
+from . import _COMBINATION, StreamIOWrapper, decrypt_setup_probe, encrypt_setup_by_gametype
 from .error import HonkyPyError
 
 
 def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--detect", "-d", action="store_true", help="Detect game decryption type as exit code.")
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    mgroup = parser.add_mutually_exclusive_group()
+    parser.add_argument("-b", "--basename", help="The actual filename of the file.")
+    mgroup.add_argument("-d", "--detect", action="store_true", help="Detect game decryption type as exit code.")
+    mgroup.add_argument(
+        "-e",
+        "--encrypt",
+        help="Perform file encryption. Default is SIF JP game file.",
+        nargs="?",
+        const=_COMBINATION[0][0].lower(),
+        choices=[c[0].lower() for c in _COMBINATION],
+    )
+    parser.add_argument("-v", "--version", type=int, default=3, help="Encryption version")
     parser.add_argument("input", help="Input file path")
     parser.add_argument("output", nargs="?", default=None, help="Output file path")
     return parser.parse_args()
 
 
-def main() -> int:
+def main_entry() -> int:
     args = get_args()
-    with open(args.input, "rb") as f:
-        if args.detect:
-            try:
+    basename = args.basename or os.path.basename(args.input)
+
+    if args.encrypt:
+        dctx = encrypt_setup_by_gametype(args.encrypt, basename, args.version)
+        with open(args.input, "rb") as f:
+            contents = f.read()
+
+        with StreamIOWrapper(open(args.output or args.input, "wb"), dctx, True) as f:
+            f.write(contents)
+    else:
+        with open(args.input, "rb") as f:
+            if args.detect:
+                try:
+                    dctx, _ = decrypt_setup_probe(args.input, f.read(16))
+                    return dctx.VERSION
+                except HonkyPyError:
+                    return 0
+            else:
                 dctx, _ = decrypt_setup_probe(args.input, f.read(16))
-                return dctx.VERSION
-            except HonkyPyError:
-                return 0
-        else:
-            dctx, _ = decrypt_setup_probe(args.input, f.read(16))
-            f.seek(dctx.HEADER_SIZE, io.SEEK_SET)
-            decrypted_data = dctx.decrypt_block(f.read())
-    with open(args.output or args.input, "wb") as f:
-        f.write(decrypted_data)
+                f.seek(dctx.HEADER_SIZE, io.SEEK_SET)
+                decrypted_data = dctx.decrypt_block(f.read())
+        with open(args.output or args.input, "wb") as f:
+            f.write(decrypted_data)
     return 0
 
 
-if __name__ == "__main__":
+def main():
     import sys
 
-    sys.exit(main())
+    sys.exit(main_entry())
+
+
+if __name__ == "__main__":
+    main()
